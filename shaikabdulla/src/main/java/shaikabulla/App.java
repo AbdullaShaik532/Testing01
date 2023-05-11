8f702659c78ce84905df42983493a6d3a87c5fad
Unais <abdullashaik227@gmail.com>
2023-05-11 06:16:48 -0400

    pom file

---
+++ shaikabdulla/src/main/java/shaikabulla/App.java
@@ -0,0 +1,13 @@
+package shaikabulla;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SHAIK ABDULLA" );
+    }
+}
